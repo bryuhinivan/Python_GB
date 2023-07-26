cbfa5328d54a57ce3a6b8900f7b361560ffeca69
bryuhin.ivan <bryuhin.ivan@gmail.com>
2023-07-26 22:24:48 +0500

    tas10 +

--- SeminarsHomeWork/task10.py
+++ SeminarsHomeWork/task10.py
@@ -3,6 +3,41 @@
 # чтобы все монетки были повернуты вверх одной и той же стороной.
 # Выведите минимальное количество монет, которые нужно перевернуть.
 
+# import random # Модуль random позволяет генерировать случайные числа.
+                # Прежде чем использовать модуль, необходимо подключить его с помощью инструкции import random
+# import math   # Этот модуль предоставляет обширный функционал для проведения вычислений
+                # с вещественными числами (числами с плавающей точкой)
+
+# while True:
+#     num = int(input('Введите количество монет на столе: '))
+#     if num == 0:
+#         print(0)
+#         break
+#     if num > 0:
+#         break
+
+# coins_array = []
+# count_one = 0
+# count_zero = 0
+
+# while num > 0:
+#     coins_array.append(random.randint(0, 1))
+#     num = num - 1
+# print(coins_array)
+
+# for i in range(0,len(coins_array)):
+#     if coins_array[i] == 0:
+#         count_zero = count_zero + 1
+#     else:
+#         count_one = count_one + 1
+
+# if count_one > count_zero:
+#     print(f'Минимальное количество монет, которое'
+#           f' надо перевернуть - {count_zero}')
+# else:
+#     print(f'Минимальное количество монет, которое'
+#           f' надо перевернуть - {count_one}')
+
 import random 
 
 while True:
